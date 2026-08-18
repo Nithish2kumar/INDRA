91147d6d61088b8c475728dd61630f9ee5cec26b
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-08-18 23:34:23 +0530

    Segmenting pcap's file data

---
+++ analysis/flow.py
@@ -0,0 +1,40 @@
+from scapy.all import rdpcap, IP, TCP,UDP
+
+def extractFlow(pcap):
+    pkt=rdpcap(pcap)
+    flows={}
+    for p in pkt:
+        if IP not in p:
+            continue
+        srcIP=p[IP].src
+        dstIP=p[IP].dst
+
+        if TCP in p:
+            sport=p[TCP].sport
+            dport=p[TCP].dport
+            proto="TCP"
+        elif UDP in p:
+            proto="UDP"
+            sport=p[TCP].sport
+            dport=p[TCP].dport
+        else:
+            continue
+
+        key=(srcIP,dstIP,sport,dport,proto)
+        if key not in flows:
+            flows[key]={
+                "src_ip":srcIP,
+                "dst_ip":dstIP,
+                "src_port":sport,
+                "dst_port":dport,
+                "proto":proto,
+                "packets":0,
+                "bytes":0,
+                "start":float(p.time),
+                "end":float(p.time)
+            }
+        flow=flows[key]
+        flow["packets"]+=1
+        flow["bytes"]+=len(p)
+        flow["end"]=float(p.time)
+    return flows
